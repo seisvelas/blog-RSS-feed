440554197d16b983bb90ff69042febe92a32f6a6
Alex Vazquez <44826516+seisvelas@users.noreply.github.com>
2021-03-28 03:41:05 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,39 @@
-use netlify_lambda_http::{
-    lambda::{lambda, Context},
-    IntoResponse, Request,
-};
+use reqwest::header::USER_AGENT;
 
-type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
-
-#[lambda(http)]
 #[tokio::main]
-async fn main(_: Request, _: Context) -> Result<impl IntoResponse, Error> {
-    Ok("🦀 Hello, Netlify 🦀")
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = reqwest::Client::new();
+    let res: Vec<serde_json::Value> = client
+        .get("https://api.github.com/users/seisvelas/gists?page=1&per_page=100")
+        .header(USER_AGENT, "Xandre's RSS")
+        .send()
+        .await?
+        .json::<Vec<serde_json::Value>>()
+        .await?;
+
+    println!("<?xml version=\"1.0\" ?>");
+    println!("  <rss version=\"2.0\">");
+    println!("      <channel>");
+    println!("          <title>Xandre's Blog</title>");
+    println!("          <link>https://seisvelas.github.io/</link>");
+    println!("          <description>Informal musings for curious spirits</description>");
+
+    for gist in &res {
+        let url = &gist["url"];
+        let desc = &gist["description"];
+
+        for (filename, _) in gist["files"].as_object().unwrap() {
+            if filename.contains("blog_") {
+                println!("          <item>");
+                println!("              <title>{}</title>", desc);
+                println!("              <link>{}</link>", url);
+                println!("              <description>{}</description>", desc);
+                println!("          </item>");
+            }
+        }
+    }
+    println!("  </channel>");
+    println!("</rss>");
+
+    Ok(())
 }
